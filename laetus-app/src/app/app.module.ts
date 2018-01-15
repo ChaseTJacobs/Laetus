869b96659b2df07546874c68f46e6da29cd16279
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2018-01-15 15:28:47 -0700

    added router dependencies and examples. Added components for home page, register page, and generic module display.

--- laetus-app/src/app/app.module.ts
+++ laetus-app/src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ModuleDisplayComponent } from './components/module-display/module-display.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
  // Any angular object MUST be put into this
  // file in order to work in the rest of the app
 @NgModule({
   declarations: [ // Components go here
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, ModuleDisplayComponent, RegisterComponent, HomeComponent
   ],
   imports: [      // Modules go here
     BrowserModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: '**',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],  // Services go here
   bootstrap: [AppComponent]
