88d4c003158cd404c1eb18af0229f62f059fd4b6
Kim Dean <dea13004@byui.edu>
2018-02-07 14:42:04 -0700

    Added component skeleton files for EVERY COMPONENT

--- laetus-app/src/app/app.module.ts
+++ laetus-app/src/app/app.module.ts
@@ -4,26 +4,55 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ModuleDisplayComponent } from './components/module-display/module-display.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/account/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterFormComponent } from './components/account/register/register-form/register-form.component';
+import { AccountSettingsComponent } from './components/account/account-settings/account-settings.component';
+import { ForgotPassComponent } from './components/account/forgot-pass/forgot-pass.component';
+import { RegisterBodyComponent } from './components/account/register/register-body/register-body.component';
+import { RegisterPaymentComponent } from './components/account/register/register-payment/register-payment.component';
+import { CalendarBodyComponent } from './components/calendar/calendar-body/calendar-body.component';
+import { CalendarDayComponent } from './components/calendar/calendar-day/calendar-day.component';
+import { CalendarDisplayComponent } from './components/calendar/calendar-display/calendar-display.component';
+import { ModuleBodyComponent } from './components/module/module-body/module-body.component';
+import { ModuleListComponent } from './components/module/module-list/module-list.component';
+import { ModuleOverviewComponent } from './components/module/module-overview/module-overview.component';
+import { ModuleGoalsComponent } from './components/module/module-display/module-goals/module-goals.component';
+import { ModuleSidebarComponent } from './components/module/module-display/module-sidebar/module-sidebar.component';
+import { ModuleViewComponent } from './components/module/module-display/module-view/module-view.component';
+import { NrmBodyComponent } from './components/nrm/nrm-body/nrm-body.component';
+import { NrmContactActivitiesComponent } from './components/nrm/nrm-contact-activities/nrm-contact-activities.component';
+import { NrmContactInfoComponent } from './components/nrm/nrm-contact-info/nrm-contact-info.component';
+import { NrmContactListComponent } from './components/nrm/nrm-contact-list/nrm-contact-list.component';
+import { NrmContactStatsComponent } from './components/nrm/nrm-contact-stats/nrm-contact-stats.component';
+import { NrmCreateActivityComponent } from './components/nrm/nrm-create-activity/nrm-create-activity.component';
+import { QuizBodyComponent } from './components/quiz/quiz-body/quiz-body.component';
+import { QuizQuestionComponent } from './components/quiz/quiz-question/quiz-question.component';
+import { QuizResultsComponent } from './components/quiz/quiz-results/quiz-results.component';
+import { StatsToolComponent } from './components/stats-tool/stats-tool.component';
 
 // Services
 import { ContactService } from './services/contact.service';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterFormComponent } from './components/register/register-form/register-form.component';
 import { AuthguardService } from './services/authguard.service';
  // Any angular object(component??) MUST be put into this
  // file in order to work in the rest of the app
 @NgModule({
   declarations: [ // Components go here
-    AppComponent, HeaderComponent, ModuleDisplayComponent, RegisterComponent, HomeComponent, LandingComponent, LoginComponent, RegisterFormComponent,
+    AppComponent, HeaderComponent, ModuleDisplayComponent, RegisterComponent, HomeComponent, LandingComponent,
+    LoginComponent, RegisterFormComponent, AccountSettingsComponent, ForgotPassComponent,
+    RegisterBodyComponent, RegisterPaymentComponent, CalendarBodyComponent, CalendarDayComponent, CalendarDisplayComponent,
+    ModuleBodyComponent, ModuleListComponent, ModuleOverviewComponent, ModuleGoalsComponent, ModuleSidebarComponent,
+    ModuleViewComponent, NrmBodyComponent, NrmContactActivitiesComponent, NrmContactInfoComponent,
+    NrmContactListComponent, NrmContactStatsComponent, NrmCreateActivityComponent, QuizBodyComponent, QuizQuestionComponent,
+    QuizResultsComponent, StatsToolComponent
   ],
   imports: [      // Modules go here
     BrowserModule,
---
+++ laetus-app/src/app/components/nrm/nrm-contact-stats/nrm-contact-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nrm-contact-stats',
+  templateUrl: './nrm-contact-stats.component.html',
+  styleUrls: ['./nrm-contact-stats.component.css']
+})
+export class NrmContactStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
