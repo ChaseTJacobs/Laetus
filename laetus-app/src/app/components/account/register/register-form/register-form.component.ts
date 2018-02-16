8769ce1f4c9365f581d2738b480ee4ea5a906ba9
Kim Dean <dea13004@byui.edu>
2018-02-16 12:55:16 -0700

    Register page front end validation and form functionality

--- laetus-app/src/app/components/account/register/register-form/register-form.component.ts
+++ laetus-app/src/app/components/account/register/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../../../../services/auth/account.service';
 
 
@@ -21,11 +21,12 @@ export class RegisterFormComponent implements OnInit {
 
 
   constructor(private fb: FormBuilder, private acctSvc: AccountService) {
+    const regex = new RegExp('^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$', 'igm');
     this.rForm = fb.group({
       'firstname': [null, Validators.required],
       'lastname': [null, Validators.required],
       'password': [null, Validators.compose([Validators.required, Validators.minLength(5)])],
-      'email': [null, Validators.required],
+      'email': [null, Validators.compose([Validators.pattern(regex), Validators.required])],
       'confirmPass': [null, Validators.required],
     });
   }
@@ -43,5 +44,22 @@ export class RegisterFormComponent implements OnInit {
   registerUser(user) {
     this.acctSvc.register(user);
   }
+
+  matchPass() {
+      if (this.rForm.controls['password'].valid && this.rForm.controls['password'].touched
+        && this.rForm.controls['confirmPass'].touched && this.rForm.controls['password'].value !== this.rForm.controls['confirmPass'].value) {
+        return true;
+      } else {
+        return false;
+  }
+
+
+
+
+
+
+
+
+
 
 }
--- laetus-app/src/app/services/http/http.service.ts
+++ laetus-app/src/app/services/http/http.service.ts
@@ -5,29 +5,28 @@ import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class HttpService {
-  
+
   public token = {
     header: null,
     data: null,
     sig: null
   }
 
-  
+
   public sToken: string;
-  
+
   getRequest(endpoint: string, param: any) {
     //this is called from the login, and sets a token
     this.setJWT("not null");
     console.log(this.sToken);
     return this.http.post('http://www.joyfulnetworking.com:3002/' + endpoint, param);
   }
-  
+
   setJWT(token: string) {
     this.sToken = token;
   }
-  
+
   getJWT(){
-    console.log("token is : " + this.sToken);
     if(this.sToken === null){
       return false;
     } else {
@@ -38,8 +37,8 @@ export class HttpService {
   logout() {
     this.sToken = null;
   }
-  
-  constructor(private http: Http) { 
+
+  constructor(private http: Http) {
     this.sToken = null;
   }
 
